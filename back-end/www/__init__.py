c8830865f2971d999f482000f3dc4dba0345f1ad
Horbin <2240045751@qq.com>
2023-05-06 15:08:33 +0800

    Update: try to change workflow. (1)

--- back-end/www/__init__.py
+++ back-end/www/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 
 from .bp_api import api
-from .bp_web import web
 
 app = Flask(__name__) # 创建app
 app.config.from_object("www.settings.DevelopmentConfig") # 配制app
 
 app.register_blueprint(api, url_prefix='/api') # 注册蓝图api
-app.register_blueprint(web) # 注册蓝图web
