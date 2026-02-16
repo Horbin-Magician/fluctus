4c73bb220dacf6deabfa8723f3bccf4416f8cab2
Horbin <2240045751@qq.com>
2026-02-16 17:49:45 +0800

    feat: add script to start front-end and back-end development servers

---
+++ dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 一键启动前后端开发服务器
+# 用法: ./dev.sh
+# Ctrl+C 同时停止前后端
+
+cleanup() {
+  echo ""
+  echo "Stopping all dev servers..."
+  kill 0
+  wait
+}
+
+trap cleanup SIGINT SIGTERM
+
+echo "Starting back-end (Flask :5000)..."
+cd back-end && uv run python application.py &
+
+echo "Starting front-end (Nuxt :3000)..."
+cd front-end && yarn dev &
+
+echo ""
+echo "Both servers running. Press Ctrl+C to stop all."
+echo ""
+
+wait
